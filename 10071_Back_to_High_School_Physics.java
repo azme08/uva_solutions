f27d09146be91a1224d99cfd58e4ce55b0734442
Abdullah Al Azme <115527810+azme08@users.noreply.github.com>
2023-09-22 21:19:48 +0600

    10071_Back_to_High_School_Physics.java

---
+++ 10071_Back_to_High_School_Physics.java
@@ -0,0 +1,19 @@
+//10071	Back to High School Physics
+
+import java.util.Scanner;
+
+public class Main {
+
+  
+    public static void main(String[] args){
+      
+      Scanner sc = new Scanner(System.in);
+		  while(sc.hasNext()){
+                    int v=sc.nextInt();
+                    int a=sc.nextInt();
+                    int res=v*2*a;
+                    System.out.println(res);
+                }
+      }
+    
+}
