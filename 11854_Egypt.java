3dd893dac03d4ad49459b203f488b8ef4df1b8dd
Abdullah Al Azme <115527810+azme08@users.noreply.github.com>
2023-09-23 12:45:07 +0600

    11854_Egypt.java

--- 11854_Egypt.java
+++ 11854_Egypt.java
@@ -1,1 +1,31 @@
+//11854	Egypt
+import java.util.Scanner;
 
+public class Main {
+
+  
+    public static void main(String[] args){
+    
+      int a,b,c;
+      
+      Scanner sc = new Scanner(System.in);
+     
+      while(sc.hasNext()){
+          a=sc.nextInt();
+          b=sc.nextInt();
+          c=sc.nextInt();
+          if(a<=0 || b<=0 || c<=0 ){
+              break;
+          }else{
+              if((a*a+b*b)==(c*c) ||(c*c+b*b)==(a*a) || (c*c+a*a)==(b*b)){
+                  System.out.println("right");
+              }else{
+                  System.out.println("wrong");
+              }
+          }
+          
+          
+      }
+    }
+    
+}
